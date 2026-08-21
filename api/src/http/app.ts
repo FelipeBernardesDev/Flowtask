84d514ec7d7e7ed56a0a71f088c2055e33b2934e
Felipe Bernardes <felipe.bernardes.dev@gmail.com>
2026-08-20 22:36:36 -0300

    docs(api): setup api documentation

--- api/src/http/app.ts
+++ api/src/http/app.ts
@@ -1,5 +1,22 @@
 import fastify from 'fastify'
+import { fastifySwagger } from '@fastify/swagger'
+import fastifyApiReference from '@scalar/fastify-api-reference'
 
 export const app = fastify({
     logger: true,
+})
+
+app.register(fastifySwagger, {
+    openapi: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Flowtask API',
+            description: 'Task management API',
+            version: '1.0.0',
+        },
+    },
+})
+
+app.register(fastifyApiReference, {
+    routePrefix: '/docs',
 })
