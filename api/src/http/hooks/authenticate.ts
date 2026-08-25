afe7b64ee3698cba0708117bee5c8ca582bba9da
Felipe Bernardes <felipe.bernardes.dev@gmail.com>
2026-08-25 10:32:23 -0300

    feat(api): add authentication hook

---
+++ api/src/http/hooks/authenticate.ts
@@ -0,0 +1,13 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+    try {
+        await request.jwtVerify({
+            onlyCookie: true,
+        })
+    } catch {
+        return reply.status(401).send({
+            message: 'Unauthorized',
+        })
+    }
+}
